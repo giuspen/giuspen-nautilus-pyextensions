d9a80b623abf03c1e2ec9cb8479227d561d9d48f
Giuseppe Penone <giuspen@gmail.com>
2021-02-25 21:42:18 +0000

    meld-compare.py, kdiff3-compare.py support for caja 1.24+ (#3)

--- default-pyextensions/meld-compare.py
+++ default-pyextensions/meld-compare.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-
 """This module adds menu items to the Caja right-click menu which allows to compare
    the selected files/folder using Meld (Diff and merge tool) just through the right-clicking"""
 
-#   meld-compare.py version 3.4
+#   meld-compare.py version 3.6
 #
-#   Copyright 2009-2014 Giuseppe Penone <giuspen@gmail.com>
+#   Copyright 2009-2021 Giuseppe Penone <giuspen@gmail.com>
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -23,14 +20,16 @@
 #   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #   MA 02110-1301, USA.
 
+import gi
+gi.require_version('Caja', '2.0')
 from gi.repository import Caja, GObject, Gtk, GdkPixbuf
-import urllib, os, subprocess, re
+import urllib.parse, os, subprocess, re
 import locale, gettext
 
 APP_NAME = "caja-pyextensions"
 LOCALE_PATH = "/usr/share/locale/"
 NAUPYEXT_MELD = 'NAUPYEXT_MELD'
-ICONPATH = "/usr/share/icons/hicolor/scalable/apps/meld.svg"
+ICONPATH = "/usr/share/icons/hicolor/16x16/apps/meld-version-control.png"
 # internationalization
 locale.setlocale(locale.LC_ALL, '')
 gettext.bindtextdomain(APP_NAME, LOCALE_PATH)
@@ -52,13 +51,18 @@ class MeldActions(GObject.GObject, Caja.MenuProvider):
             factory.add_default()
         except: pass
 
-    def run(self, menu, element_1, element_2):
+    def _run(self, menu, element_1, element_2):
         """Runs the Meld Comparison of selected files/folders"""
-        subprocess.call("meld %s %s &" % (element_1, element_2), shell=True)
+        subprocess.call("meld %s %s &" % (re.escape(element_1), re.escape(element_2)), shell=True)
 
-    def meld_save(self, menu, element):
+    def _meld_save(self, menu, element):
         """Save the File/Folder Path for Future Use"""
         os.environ[NAUPYEXT_MELD] = element
+
+    def _is_text_document(self, filepath):
+        """The given filepath is a text document"""
+        filetype = subprocess.Popen("file -i %s" % re.escape(filepath), shell=True, stdout=subprocess.PIPE).communicate()[0]
+        return (b"text" in filetype or b"xml" in filetype or b"csv" in filetype)
 
     def get_file_items(self, window, sel_items):
         """Adds the 'Add To Audacious Playlist' menu item to the Caja right-click menu,
@@ -67,34 +71,27 @@ class MeldActions(GObject.GObject, Caja.MenuProvider):
         if num_paths == 0 or num_paths > 2: return
         uri_raw = sel_items[0].get_uri()
         if len(uri_raw) < 7: return
-        element_1 = urllib.unquote(uri_raw[7:])
+        element_1 = urllib.parse.unquote(uri_raw[7:])
         if num_paths == 2:
             uri_raw = sel_items[1].get_uri()
             if len(uri_raw) < 7: return
-            element_2 = urllib.unquote(uri_raw[7:])
+            element_2 = urllib.parse.unquote(uri_raw[7:])
             if os.path.isfile(element_1):
                 if not os.path.isfile(element_2): return
-                element_1 = re.escape(element_1)
-                filetype = subprocess.Popen("file -i %s" % element_1, shell=True, stdout=subprocess.PIPE).communicate()[0]
-                if "text" not in filetype and "xml" not in filetype: return
-                element_2 = re.escape(element_2)
-                filetype = subprocess.Popen("file -i %s" % element_2, shell=True, stdout=subprocess.PIPE).communicate()[0]
-                if "text" not in filetype and "xml" not in filetype: return
+                if not self._is_text_document(element_1): return
+                if not self._is_text_document(element_2): return
             elif os.path.isdir(element_1):
                 if not os.path.isdir(element_2): return
-                element_1 = re.escape(element_1)
-                element_2 = re.escape(element_2)
             else: return
             item = Caja.MenuItem(name='Meld::meld',
                                      label=_('Meld Compare'),
                                      tip=_('Compare the selected Files/Folders using Meld (Diff and merge tool)'),
                                      icon='meld')
-            item.connect('activate', self.run, element_1, element_2)
+            item.connect('activate', self._run, element_1, element_2)
             return item,
         # only one item selected
-        if os.path.isfile(element_1):
-            filetype = subprocess.Popen("file -i %s" % re.escape(element_1), shell=True, stdout=subprocess.PIPE).communicate()[0]
-            if "text" not in filetype and "xml" not in filetype: return
+        if os.path.isfile(element_1) and not self._is_text_document(element_1):
+            return
         # top menuitem
         top_menuitem = Caja.MenuItem(name='Meld::actions',
                                          label=_('Meld Actions'),
@@ -108,7 +105,7 @@ class MeldActions(GObject.GObject, Caja.MenuProvider):
                                               label=_('Save Path for Future Use'),
                                               tip=_('Save the Selected File/Dir Path for Future Use'),
                                               icon='gtk-save')
-        sub_menuitem_save.connect('activate', self.meld_save, element_1)
+        sub_menuitem_save.connect('activate', self._meld_save, element_1)
         submenu.append_item(sub_menuitem_save)
         # submenu items compare with saved
         stored_path = os.environ[NAUPYEXT_MELD] if NAUPYEXT_MELD in os.environ else ""
@@ -117,6 +114,6 @@ class MeldActions(GObject.GObject, Caja.MenuProvider):
                                                            label=_('Compare with %s' % stored_path.replace("_", " ") ),
                                                            tip=_('Compare the Selected File/Dir with %s' % stored_path),
                                                            icon='gtk-execute')
-            sub_menuitem_compare_saved.connect('activate', self.run, re.escape(element_1), re.escape(stored_path))
+            sub_menuitem_compare_saved.connect('activate', self._run, element_1, stored_path)
             submenu.append_item(sub_menuitem_compare_saved)
         return top_menuitem,
